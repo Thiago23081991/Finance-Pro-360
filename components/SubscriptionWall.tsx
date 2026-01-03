d3afec0fad18d32d350cf9819970a521c9644c42
Thiago FinancePro <thiago@financepro.com>
2026-01-03 18:56:48 -0300

    fix: explicit useState import to resolve build error

--- components/SubscriptionWall.tsx
+++ components/SubscriptionWall.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 import { Rocket, Check, Zap, Copy, MessageCircle } from 'lucide-react';
 import { QRCodeSVG } from 'qrcode.react';
 import { Logo } from './Logo';
